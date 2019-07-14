340a8e331a4447d1bc0a32907966d3cd13938663
DmJSCourse <dimonua@gmail.com>
2019-07-14 13:43:40 +0300

    v2

--- l6.js
+++ l6.js
@@ -2,4 +2,7 @@ $( "#add" ).click(addCat);
 
 function addCat() {
     $( "#cats" ).after( '<div class="itembox" style="float: left;">' + $("#name").val() + " " + "(" + $("#age").val() + ')<br><img src="' + $("#link").val() + '"></div>');
+    $("#name").val('');
+    $("#age").val('');
+    $("#link").val('');
 };
